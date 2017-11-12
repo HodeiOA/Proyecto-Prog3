b5ccf7f0a419451b7dd0d87574b8222b9d698a31
Hodei Olaskoaga <hodei.olaskoaga@opendeusto.es>
2017-11-12 09:59:06 +0100

    Hey there, we are using GitHub!

---
+++ src/LN/PruebaLN.java
@@ -0,0 +1,11 @@
+package LN;
+
+public class PruebaLN 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	}
+
+}
---
+++ src/LP/PruebaLP.java
@@ -0,0 +1,7 @@
+package LP;
+
+public class PruebaLP 
+{
+	// Yo sirvo para ver si los paquetes se suben a GitHub
+	
+}
